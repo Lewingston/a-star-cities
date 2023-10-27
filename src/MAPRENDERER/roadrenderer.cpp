ef8140aa59560dd6ad487e7a9564d89d6fcc1c91
Lewingston <jonas.speyer@web.de>
2023-10-27 23:36:46 +0200

    Removed outcommented code

--- src/MAPRENDERER/roadrenderer.cpp
+++ src/MAPRENDERER/roadrenderer.cpp
@@ -15,8 +15,6 @@ RoadRenderer::RoadRenderer(const Road& road, sf::Color color) :
     road(road),
     line(sf::PrimitiveType::LineStrip, road.getNodes().size()) {
 
-    //const sf::Color color = getColorByType(road.getType());
-
     const std::vector<std::reference_wrapper<const Node>>& nodes = road.getNodes();
     for (std::size_t ii = 0; ii < nodes.size(); ii++) {
         const auto [posX, posY] = nodes.at(ii).get().getLocalPosition();
@@ -31,40 +29,3 @@ void RoadRenderer::draw(std::shared_ptr<sf::RenderTarget> renderTarget, const sf
     //renderStates.shader    = &shader;
     renderTarget->draw(line, renderStates);
 }
-
-/*sf::Color RoadRenderer::getColorByType(RoadType type) const {
-
-    const uint8_t r = std::rand() % 255;
-    const uint8_t g = std::rand() % 255;
-    const uint8_t b = std::rand() % 255;
-
-    return sf::Color(r, g, b);
-
-    switch (type.getEnumValue()) {
-        case  RoadType::LIVING_STREET:
-            return sf::Color(16, 16, 16);
-        case RoadType::RESIDENTIAL:
-            return sf::Color(18, 18, 18);
-        case RoadType::UNCLASSIFIED:
-            return sf::Color(20, 20, 20);
-        case RoadType::TERTIARY:
-        case RoadType::TERTIARY_LINK:
-            return sf::Color(22, 22, 22);
-        case RoadType::SECONDARY:
-        case RoadType::SECONDARY_LINK:
-            return sf::Color(24, 24, 24);
-        case RoadType::PRIMARY:
-        case RoadType::PRIMARY_LINK:
-            return sf::Color(26, 26, 26);
-        case RoadType::TRUNK:
-        case RoadType::TRUNK_LINK:
-            return sf::Color(28, 28, 28);
-        case RoadType::MOTORWAY:
-        case RoadType::MOTORWAY_LINK:
-            return sf::Color(30, 30, 30);
-        default:
-            return sf::Color(255, 0, 255);
-    }
-
-    return sf::Color(255, 0, 255);
-}*/
