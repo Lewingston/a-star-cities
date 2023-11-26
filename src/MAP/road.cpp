3d5cf91d3513f9275d05e7efe7a96aad8b59dcda
Lewingston <jonas.speyer@web.de>
2023-11-26 14:09:31 +0100

    Add interstions as start end end point of road

--- src/MAP/road.cpp
+++ src/MAP/road.cpp
@@ -1,8 +1,25 @@
 
 #include "road.h"
 
+#include <iostream>
+
 using namespace AStarCities;
 
 void Road::setNodes(const std::vector<std::reference_wrapper<const Node>>& nodes) {
     this->nodes = nodes;
+}
+
+std::pair<const Intersection&, const Intersection&> Road::getIntersections() const {
+    return {*startIntersection, *endIntersection};
+}
+
+void Road::setEndPoint(Intersection* intersection) {
+
+    if (startIntersection == nullptr) {
+        startIntersection = intersection;
+    } else if (endIntersection == nullptr) {
+        endIntersection = intersection;
+    } else {
+        std::cerr << "Map - Error: Map end points are allready set. Something went wrong\n";
+    }
 }
--- src/MAP/road.h
+++ src/MAP/road.h
@@ -9,6 +9,7 @@
 namespace AStarCities {
 
     class Node;
+    class Intersection;
 
     class Road {
 
@@ -18,11 +19,20 @@ namespace AStarCities {
 
             virtual ~Road() = default;
 
+            bool operator<(const Road& road) const { return road.id < id; }
+
             void setNodes(const std::vector<std::reference_wrapper<const Node>>& nodes);
+
+            void setEndPoint(Intersection* intersection);
 
             [[nodiscard]] uint64_t    getId()   const noexcept { return id; }
             [[nodiscard]] std::string getName() const noexcept { return name; }
             [[nodiscard]] RoadType    getType() const noexcept { return type; }
+
+            [[nodiscard]] const Node& getStartNode() const { return nodes.begin()->get(); }
+            [[nodiscard]] const Node& getEndNode() const { return (nodes.end() - 1)->get(); }
+
+            [[nodiscard]] std::pair<const Intersection&, const Intersection&> getIntersections() const;
 
             [[nodiscard]] const std::vector<std::reference_wrapper<const Node>>& getNodes() const { return nodes; }
 
@@ -35,6 +45,9 @@ namespace AStarCities {
             RoadType type;
 
             std::vector<std::reference_wrapper<const Node>> nodes;
+
+            Intersection* startIntersection = nullptr;
+            Intersection* endIntersection = nullptr;
 
     };
 }
