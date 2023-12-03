c7f391db8c1b1b2a121a70f6377f00fa77b1019c
Lewingston <jonas.speyer@web.de>
2023-12-03 15:10:16 +0100

    Implemented A* path finding algorithm

--- src/SOLVER/solver.cpp
+++ src/SOLVER/solver.cpp
@@ -7,6 +7,12 @@
 
 using namespace AStarCities;
 
+Solver::Solver(std::shared_ptr<Map> map, const Intersection& start, const Intersection& end) :
+    map(map), startNode(start), endNode(end) {
+
+    init();
+    }
+
 std::pair<const Intersection&, const Intersection&> Solver::selectStartAndEndIntersection(std::shared_ptr<Map> map) {
 
     double minDistance = std::max(map->getLocalWidth(), map->getLocalWidth()) / 2;
@@ -24,7 +30,7 @@ std::pair<const Intersection&, const Intersection&> Solver::selectStartAndEndInt
             continue;
         }
 
-        if (minDistance < getDistance(start, end)) {
+        if (minDistance < start.getNode().localDistance(end.getNode())) {
             std::cout << "Tries: " << tries << "\n";
             return {start, end};
         }
@@ -47,10 +53,159 @@ const Intersection& Solver::selectRandomIntersection(std::shared_ptr<Map> map) {
     return iter->second;
 }
 
-double Solver::getDistance(const Intersection& inter1, const Intersection& inter2) {
+void Solver::init() {
 
-    const auto [x1, y1] = inter1.getPosition();
-    const auto [x2, y2] = inter2.getPosition();
+    openList.clear();
+    closedList.clear();
+    nodes.clear();
 
-    return std::sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    // init path nodes
+    // TODO: it is ineffective to generate all elements at the start
+    // maybe there is a better solution
+    for (const auto& [id, inter] : map->getIntersections()) {
+        nodes.insert({id, PathNode(inter)});
+    }
+
+    // init open list
+    PathNode& start = nodes.find(startNode.getId())->second;
+    start.setDistanceToTarget(startNode.getNode().localDistance(endNode.getNode()));
+    start.setDistanceTraveled(0);
+    openList.insert(start);
+}
+
+void Solver::doStep(bool doSubSteps) {
+
+    if (solved)
+        return;
+
+    if (openList.empty()) {
+        // TODO: algorithm allready terminated
+        return;
+    }
+
+    // get the node with the lowest score from the open list and remove it
+    PriorityQueue::iterator iter = openList.begin();
+    PathNode& currentNode = iter->get();
+
+    if (currentNode.getIntersection() == endNode) {
+        this->currentNode = nullptr;
+        solved = true;
+        return;
+    }
+
+    openList.erase(iter);
+
+    this->currentNode = &currentNode;
+
+    closedList.insert(currentNode);
+
+    currentConnections = currentNode.getConnections();
+    currentConnectionIterator = currentConnections.begin();
+
+    if (!doSubSteps)
+        return;
+
+    //expandNode(currentNode);
+
+    for (const Intersection::Connection& connection : currentConnections) {
+        doSubStep(currentNode, connection);
+    }
+
+    if (openList.empty()) {
+        // TODO: no solution found
+        return;
+    }
+}
+
+Road const* Solver::doSubStep() {
+
+    if (currentNode == nullptr)
+        doStep(false);
+
+    if (solved)
+        return nullptr;
+
+    doSubStep(*currentNode, *currentConnectionIterator);
+
+    Road const* road = &currentConnectionIterator->road;
+
+    currentConnectionIterator++;
+
+    if (currentConnectionIterator == currentConnections.end())
+        currentNode = nullptr;
+
+    return road;
+}
+
+void Solver::doSubStep(const PathNode& currentNode, const Intersection::Connection& connection) {
+
+    PathNode& nextNode = nodes.find(connection.intersection.getId())->second;
+
+    if (closedList.contains(nextNode))
+        return;
+
+    double newDistance = connection.road.getLocalLength() + currentNode.getDistanceTraveled();
+
+    if (openList.contains(nextNode) && newDistance >= nextNode.getDistanceTraveled())
+        return;
+
+    nextNode.setPredecessor(currentNode, connection.road);
+    nextNode.setDistanceTraveled(newDistance);
+    const double distanceToTarget = connection.intersection.getNode().localDistance(endNode.getNode());
+    nextNode.setDistanceToTarget(distanceToTarget);
+
+    // Elements in a set are constant. Therefor the element has to be removed
+    // and reinserted for the key to be updated
+    auto [iter, success] = openList.insert(nextNode);
+    if (!success) {
+        //std::cout << "Update node in open list: " << iter->get().getId() << '\n';
+        openList.erase(iter);
+        openList.insert(nextNode);
+    } else {
+        //std::cout << "Enter new node in open list: " << connection.intersection.getId() << '\n';
+    }
+}
+
+std::vector<std::reference_wrapper<const Road>> Solver::getSolution() const {
+
+    std::vector<std::reference_wrapper<const Road>> solution;
+
+    std::reference_wrapper<const PathNode> currentNode = openList.begin()->get();
+
+    while (currentNode.get().getId() != startNode.getId()) {
+        if (currentNode.get().getRoadToPredecessor() == nullptr) {
+            std::cerr << "Solver: ERROR - Node hast no road to predecessor.\n";
+            break;
+        }
+
+        solution.push_back(*currentNode.get().getRoadToPredecessor());
+
+        if (currentNode.get().getPredecessor() == nullptr) {
+            std::cerr << "Solver: ERROR - Node hat no predecessor.\n";
+        }
+
+        currentNode = *currentNode.get().getPredecessor();
+    }
+
+    return solution;
+}
+
+void Solver::printOpenList() {
+    std::cout << "\nOpen List (" << openList.size() << "):\n";
+    for (const PathNode& node : openList) {
+        //std::cout << node.getId() << " - " << node.getScore() << '\n';
+        std::cout << node.getId() << " - traveled: " << node.getDistanceTraveled() << " - distance: " << node.getDistanceToTarget() << '\n';
+    }
+    std::cout << '\n';
+}
+
+std::vector<std::reference_wrapper<const Intersection>> Solver::getOpenListIntersections() const {
+
+    std::vector<std::reference_wrapper<const Intersection>> intersections;
+
+    for (const PathNode& node : openList) {
+        intersections.push_back(node.getIntersection());
+    }
+
+    return intersections;
 }
--- src/SOLVER/solver.h
+++ src/SOLVER/solver.h
@@ -2,30 +2,111 @@
 
 #include "MAP/map.h"
 
+#include <limits>
+
 namespace AStarCities {
 
     class Solver {
 
         public:
 
-            Solver(std::shared_ptr<Map> map, const Node& startNode, const Node& endNode) :
-                map(map), startNode(startNode), endNode(endNode) {}
+            Solver(std::shared_ptr<Map> map, const Intersection& startNode, const Intersection& endNode);
 
             virtual ~Solver() = default;
 
             static std::pair<const Intersection&, const Intersection&> selectStartAndEndIntersection(std::shared_ptr<Map> map);
 
+            const Intersection& getStart() const { return startNode; }
+            const Intersection& getEnd()   const { return endNode; }
+
+            void doStep(bool doSubSteps = true);
+            Road const* doSubStep();
+
+            [[nodiscard]] bool isDone() const { return solved; }
+
+            [[nodiscard]] std::vector<std::reference_wrapper<const Road>> getSolution() const;
+
+            [[nodiscard]] std::size_t getOpenListSize() const { return openList.size(); }
+
+            void printOpenList();
+
+            [[nodiscard]] std::vector<std::reference_wrapper<const Intersection>> getOpenListIntersections() const;
+
         private:
+
+            class PathNode {
+
+                public:
+
+                    PathNode(const Intersection& inter) :
+                        intersection(inter) {}
+
+                    virtual ~PathNode() = default;
+
+                    bool operator<(const PathNode& node) const {
+                        if (node.getIntersection() == getIntersection()) {
+                            return false;
+                        } if (node.getScore() == getScore()) {
+                            return node.getIntersection() < getIntersection();
+                        } else {
+                            return node.getScore() > getScore();
+                        }
+                    }
+
+                    [[nodiscard]] uint64_t getId() const { return intersection.getId(); }
+
+                    [[nodiscard]] const Intersection& getIntersection() const { return intersection; }
+                    [[nodiscard]] Road const* getRoadToPredecessor() const { return roadToPrev; }
+                    [[nodiscard]] PathNode const* getPredecessor() const { return prevNode; }
+
+                    [[nodiscard]] double getDistanceToTarget() const { return distanceToTarget; }
+                    [[nodiscard]] double getDistanceTraveled() const { return distanceTraveled; }
+
+                    [[nodiscard]] double getScore() const { return getDistanceToTarget() + getDistanceTraveled(); }
+
+                    void setDistanceToTarget(double distance) { distanceToTarget = distance; }
+                    void setDistanceTraveled(double distance) { distanceTraveled = distance; }
+
+                    void setPredecessor(const PathNode& prev, const Road& road) {
+                        prevNode = &prev;
+                        roadToPrev = &road;
+                    }
+
+                    [[nodiscard]] std::vector<Intersection::Connection> getConnections() const { return intersection.getConnections(); }
+
+                private:
+
+                    const Intersection& intersection;
+                    PathNode const* prevNode = nullptr;
+                    Road const* roadToPrev = nullptr;
+
+                    double distanceTraveled = std::numeric_limits<double>::max();
+                    double distanceToTarget = std::numeric_limits<double>::max();
+            };
 
             static const Intersection& selectRandomIntersection(std::shared_ptr<Map> map);
 
-            static double getDistance(const Intersection& iter1, const Intersection& iter2);
+            void init();
+
+            void doSubStep(const PathNode& currentNode, const Intersection::Connection& connection);
 
             std::shared_ptr<Map> map;
 
-            const Node& startNode;
-            const Node& endNode;
+            std::map<uint64_t, PathNode> nodes;
 
+            using PriorityQueue = std::set<std::reference_wrapper<PathNode>, std::less<PathNode>>;
+
+            PriorityQueue openList;
+            std::set<std::reference_wrapper<PathNode>, std::less<PathNode>> closedList;
+
+            const Intersection& startNode;
+            const Intersection& endNode;
+
+            PathNode* currentNode = nullptr;
+            std::vector<Intersection::Connection> currentConnections;
+            std::vector<Intersection::Connection>::iterator currentConnectionIterator;
+
+            bool solved = false;
 
     };
 }
