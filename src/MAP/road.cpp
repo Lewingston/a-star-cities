fd76111a4f42275a907c7be477483f83a95c33f6
Lewingston <jonas.speyer@web.de>
2023-12-03 15:07:18 +0100

    Implemented function to calculate length of road

--- src/MAP/road.cpp
+++ src/MAP/road.cpp
@@ -1,5 +1,6 @@
 
 #include "road.h"
+#include "node.h"
 
 #include <iostream>
 
@@ -7,6 +8,8 @@ using namespace AStarCities;
 
 void Road::setNodes(const std::vector<std::reference_wrapper<const Node>>& nodes) {
     this->nodes = nodes;
+    localLength = calculateLength(false);
+    globalLength = calculateLength(true);
 }
 
 std::pair<const Intersection&, const Intersection&> Road::getIntersections() const {
@@ -22,4 +25,23 @@ void Road::setEndPoint(Intersection* intersection) {
     } else {
         std::cerr << "Map - Error: Map end points are allready set. Something went wrong\n";
     }
+}
+
+double Road::calculateLength(bool global) const {
+
+    double length = 0;
+
+    auto currentNode = nodes.begin();
+
+    for (auto nextNode = nodes.begin() + 1; nextNode != nodes.end(); nextNode++) {
+
+        if (!global)
+            length += currentNode->get().localDistance(*nextNode);
+        else
+            length += currentNode->get().globalDistance(*nextNode);
+
+        currentNode = nextNode;
+    }
+
+    return length;
 }
--- src/MAP/road.h
+++ src/MAP/road.h
@@ -32,11 +32,16 @@ namespace AStarCities {
             [[nodiscard]] const Node& getStartNode() const { return nodes.begin()->get(); }
             [[nodiscard]] const Node& getEndNode() const { return (nodes.end() - 1)->get(); }
 
+            [[nodiscard]] double getGlobalLength() const { return globalLength; }
+            [[nodiscard]] double getLocalLength()  const { return localLength; }
+
             [[nodiscard]] std::pair<const Intersection&, const Intersection&> getIntersections() const;
 
             [[nodiscard]] const std::vector<std::reference_wrapper<const Node>>& getNodes() const { return nodes; }
 
         private:
+
+            [[nodiscard]] double calculateLength(bool global) const;
 
             uint64_t id;
 
@@ -45,6 +50,9 @@ namespace AStarCities {
             RoadType type;
 
             std::vector<std::reference_wrapper<const Node>> nodes;
+
+            double localLength;
+            double globalLength;
 
             Intersection* startIntersection = nullptr;
             Intersection* endIntersection = nullptr;
